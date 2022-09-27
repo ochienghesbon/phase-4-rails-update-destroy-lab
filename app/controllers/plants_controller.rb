2ea469818056886bdad7a4a02416abc63fb187b5
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-09-27 13:08:28 +0300

    Done.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,8 +17,22 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  # PATCH /plants/:id
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  end
+  def destroy
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end
 
   private
+  def find_plant
+    Plant.find(params[:id])
+  end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
